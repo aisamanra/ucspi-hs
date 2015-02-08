bc631db0e0099843444cb8f531774a116dcdd4d9
Getty Ritter <gettylefou@gmail.com>
2015-02-07 16:22:22 -0800

    Documentation added

--- Network/UCSPI.hs
+++ Network/UCSPI.hs
@@ -1,12 +1,25 @@
-module Network.UCSPI where
+module Network.UCSPI
+ ( UCSPIOptions(..)
+ , ucspiClient
+ , ucspiServer
+ ) where
 
 import Control.Arrow (first)
 import Data.List (stripPrefix)
-import System.IO (Handle, hPutStrLn, stderr)
+import System.IO (Handle, hClose, hPutStrLn, stderr)
 import System.Posix.IO (fdToHandle)
 import System.Posix.Types (Fd(..))
 import System.Environment (getEnvironment)
 
+-- | The @UCSPI@ spec indicates that applications will receive information
+--   about their connection through environment variables. A 'UCSPIOptions'
+--   value will contains all the relevant values filtered out. As most of
+--   these (save for the protocol name) are prefixed with both the protocol
+--   name and either @LOCAL@ or @REMOTE@, those values are stripped before
+--   they are added to the respective lists: for example, if a @UCSPI@
+--   application for the @TCP@ protocol passes in an environment variable
+--   @TCPREMOTEPORT=7777@, then the value of 'ucspiProtocol' will be
+--   @TCP@ and 'ucspiRemoteVars' will contain a pair @(\"PORT\","7777")@.
 data UCSPIOptions = UCSPIOptions
   { ucspiProtocol   :: String
   , ucspiLocalVars  :: [(String,String)]
@@ -29,6 +42,17 @@ gatherOptions envs = do
              }
 
 
+-- | A @UCSPI@ client is passed an options value, a handle which corresponds
+--   to the reading end of a socket, and a handle which corresponds to the
+--   writing end of a socket. For example, a sample @UCSPI@ client which
+--   connects to a server, writes a line, and reads something back, regardless
+--   of the underlying transport mechanism, would look like
+--
+--   > main :: IO ()
+--   > main = ucspiClient $ \_ rdH wrH -> do
+--   >   hPutStrLn wrH "hello"
+--   >   ln <- hGetLine rdH
+--   >   putStrLn ln
 ucspiClient :: (UCSPIOptions -> Handle -> Handle -> IO ()) -> IO ()
 ucspiClient client = do
   opts <- gatherOptions `fmap` getEnvironment
@@ -38,7 +62,16 @@ ucspiClient client = do
       rdH <- fdToHandle (Fd 6)
       wrH <- fdToHandle (Fd 7)
       client opts' rdH wrH
+      hClose rdH
+      hClose wrH
 
+-- | A @UCSPI@ server is passed only an options value. In order to read from or
+--   write to the connection, it just uses @stdin@ and @stdout@, which means that
+--   a server which reads a single line from a client and echoes it back would
+--   look like
+--
+-- > main :: IO ()
+-- > main = ucspiServer $ \_ -> getLine >>= putStrLn
 ucspiServer :: (UCSPIOptions -> IO ()) -> IO ()
 ucspiServer server = do
   opts <- gatherOptions `fmap` getEnvironment
